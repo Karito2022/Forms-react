1b4bd2e940a0e93b07b7fc169ee38364f35cad8b
Karito2022 <reyesbarrerakaren@gmail.com>
2022-04-12 18:50:07 -0500

    Finalized

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="card m-5">
+          <div className="card-body">
+            <Form />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default App;
